4ee695e9d9fcc6b3978ca403b8fe28b43b659b03
fatihmuhamadridho <fatihmuhamadridho1@gmail.com>
2022-01-22 23:26:09 +0700

    onepagescroll

--- onepagescroll/pages/index.tsx
+++ onepagescroll/pages/index.tsx
@@ -1,13 +1,37 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <h1>Om Asmun</h1>
-      <h1>{2020-1979}</h1>
+    <div>
+      <Head>
+        <meta charSet='UTF-8' />
+        <title>One Page Website Layout</title>
+      </Head>
+      <nav>
+        <ul>
+          <li><a href="#section1">Home</a></li>
+          <li><a href="#section2">News</a></li>
+          <li><a href="#section3">Blog</a></li>
+          <li><a href="#section4">About</a></li>
+          <li><a href="#section5">Contact</a></li>
+        </ul>
+      </nav>
+      <div id='section1'>
+        <h1>This is Home section</h1>
+      </div>
+      <div id='section2'>
+        <h1>This is News section</h1>
+      </div>
+      <div id='section3'>
+        <h1>This is Blog section</h1>
+      </div>
+      <div id='section4'>
+        <h1>This is About section</h1>
+      </div>
+      <div id='section5'>
+        <h1>This is Contact section</h1>
+      </div>
     </div>
   )
 }
