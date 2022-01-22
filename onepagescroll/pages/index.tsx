ce59406b77c8c010d43aeb4cc177e4735ae2a8fa
fatihmuhamadridho <fatihmuhamadridho1@gmail.com>
2022-01-22 23:24:39 +0700

    first commit

---
+++ onepagescroll/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Om Asmun</h1>
+      <h1>{2020-1979}</h1>
+    </div>
+  )
+}
+
+export default Home
